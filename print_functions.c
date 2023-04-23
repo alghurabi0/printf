11036a1fd67e37c4afa0c89a231d7016a5ca9436
alghurabi0 <zaid.alghurabi01@gmail.com>
2023-04-22 17:27:45 -0700

    intergr

--- print_functions.c
+++ print_functions.c
@@ -44,4 +44,74 @@ int print_percent(__attribute__((unused)) va_list args)
 {
 return (_putchar('%'));
 }
+/**
+ * print_integer - prints an integer
+ * @args: the integer to print
+ * Return: the number of characters printed
+ */
+int print_integer(va_list args)
+{
+	int n = va_arg(args, int);
+	int count = 0;
 
+	if (n < 0)
+	{
+		count += _putchar('-');
+		n = -n;
+	}
+
+	if (n / 10)
+		count += print_integer(args);
+	count += _putchar((n % 10) + '0');
+	return (count);
+}
+/**
+ * _printf - prints output according to a format
+ * @format: character string containing zero or more directives
+ *
+ * Return: the number of characters printed
+ * (excluding the null byte used to end output to strings)
+ */
+int _printf(const char *format, ...)
+{
+va_list args;
+int i = 0, count = 0;
+int j;
+format_t formats[] = {
+{"c", print_char},
+{"s", print_string},
+{"%", print_percent},
+{"d", print_integer},
+{"i", print_integer},
+{NULL, NULL}
+};
+
+va_start(args, format);
+while (format && format[i])
+{
+	if (format[i] == '%')
+	{
+		i++;
+		j = 0;
+		while (formats[j].type)
+		{
+			if (format[i] == *(formats[j].type))
+			{
+				count += formats[j].func(args);
+				break;
+			}
+			j++;
+		}
+		if (!formats[j].type && format[i])
+		{
+			count += _putchar('%');
+			count += _putchar(format[i]);
+		}
+	}
+	else
+		count += _putchar(format[i]);
+	i++;
+}
+va_end(args);
+return (count);
+}
